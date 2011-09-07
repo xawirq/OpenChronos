4746498e231a16124db16f0f4e69bcc10d9fdae7
Dobfek <dobfek@hotmail.com>
2011-09-07 23:25:33 +0200

    Eco Display: Lower LCD freq at night(19h-08h), lowers current consumption by 30%.

--- driver/display.h
+++ driver/display.h
@@ -370,5 +370,7 @@ extern u8 * itoa(u32 n, u8 digits, u8 blanks);
 extern u8 switch_seg(u8 line, u8 index1, u8 index2);
 
 void display_all_off(void);
-
+#ifdef ECO_DISPLAY 
+void eco_display(void); //ECO display
+#endif
 #endif // __DISPLAY_
